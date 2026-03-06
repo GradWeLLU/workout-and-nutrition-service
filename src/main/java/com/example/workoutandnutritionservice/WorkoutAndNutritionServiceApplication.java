b0aec3248430d6d11f37ce0cd49e2c3ce28f4d80
Rola Hadidy <rolatotta2004@gmail.com>
2026-03-06 22:39:49 +0200

    Initial commit

---
+++ src/main/java/com/example/workoutandnutritionservice/WorkoutAndNutritionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.workoutandnutritionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutAndNutritionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutAndNutritionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workoutandnutritionservice/WorkoutAndNutritionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutandnutritionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAndNutritionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
